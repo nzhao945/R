32cfa6db8ad15b114d7fdc88f927f419f7be323f
nzhao945 <nzh0321@163.com>
2017-12-31 15:01:20 +0800

    加载data.table包

--- test.R
+++ test.R
@@ -1,9 +1,12 @@
-rst <- c(rep("chr",ncol(x)))
-for (i in 1:ncol(x)) {
-    for (j in 1:nrow(x)) {
-    if (x[j,i]>0)  rst[i]<-colnames(x[i])
-    #print(colnames(x[i]))
-  }
-  
-}
-print(rst)
+# rst <- c(rep("chr",ncol(x)))
+# for (i in 1:ncol(x)) {
+#     for (j in 1:nrow(x)) {
+#     if (x[j,i]>0)  rst[i]<-colnames(x[i])
+#     #print(colnames(x[i]))
+#   }
+#   
+# }
+# print(rst)
+library(data.table)
+mtcars_dt <- data.table(mtcars)
+mtcars_dt[1:10]
