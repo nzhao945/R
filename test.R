3270a817fe3c12a65746561b294b680cb6cd479b
nzhao945 <nzh0321@163.com>
2018-01-01 20:28:30 +0800

    state line(dt)

--- test.R
+++ test.R
@@ -11,3 +11,4 @@ library(data.table)
 mtcars_dt <- data.table(mtcars)
 temp <- mtcars_dt[1:10]
 tmp <- copy(temp)
+dt <- data.table(x=rep(c("a","b","c"),times=3),y=1:9,z=rep(1:3,each=3),stringsAsFactors = F)
