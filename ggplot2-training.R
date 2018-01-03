a95c0472c1bcc92c3242acb213dd204b9ce20475
nzhao945 <nzh0321@163.com>
2018-01-03 16:05:17 +0800

    aes的参数有哪些？实践出真知，多看文档

--- ggplot2-training.R
+++ ggplot2-training.R
@@ -1,4 +1,7 @@
 library(ggplot2)
-p <- ggplot(mtcars,aes(mpg,wt,col=cyl))+
-  geom_point()
-  # +geom_smooth()
+p <- ggplot(mtcars,mapping=aes(mpg,wt,col=cyl))+
+  geom_point()  #设定散点的颜色为蓝色 geom_point(col="blue) 
+  ##geom_point(aes(col="blue"))  错误的映射关系，
+  ##在aes中, color = “blue”的实际意思是把”blue”当为一个变量，按默认的颜色标度标记为桃红色
+   # +geom_smooth()
+p
