6d4f4fa3a98d8421c62e3aa471c283eb3d319447
nzhao945 <nzh0321@163.com>
2018-01-04 15:02:46 +0800

    aes

--- RfuntionTrial/ggplot2-training.R
+++ RfuntionTrial/ggplot2-training.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 p <- ggplot(mtcars,mapping=aes(mpg,wt,col=factor(cyl)))+
   geom_point()  #设定散点的颜色为蓝色 geom_point(col="blue") 
-  ##geom_point(aes(col="blue"))  错误的映射关系，至少也要写在aes函数外，
+  ##geom_point(aes(col="blue"))  错误的映射关系，“在aes内colour等于向量，在外等于颜色”
   ##在aes中, color = “blue”的实际意思是把”blue”当为一个变量，按默认的颜色标度标记为桃红色
    # +geom_smooth()
 p
