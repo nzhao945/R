0e6aad1891befee8505b643ee3351d24bbc8fc18
nzhao945 <nzh0321@163.com>
2018-01-11 13:35:05 +0800

    kaggle

--- kaggle_data/Titanic Machine Learning from Disaster/titanic_survival.R
+++ kaggle_data/Titanic Machine Learning from Disaster/titanic_survival.R
@@ -1,11 +1,24 @@
 # 本项目需要加载的包
 library(data.table)
+library(dplyr)
 library(mice)
 library(ggplot2)
 library(randomForest)
 
 #导入训练集和测试集
-titanic_train <- fread(header = T,stringsAsFactors = F,
+train <- fread(header = T,stringsAsFactors = F,
                 file = "G:/R/kaggle_data/Titanic Machine Learning from Disaster/train.csv")
-titanic_test <- fread(header = T,stringsAsFactors = F,
+test <- fread(header = T,stringsAsFactors = F,
                 file = "G:/R/kaggle_data/Titanic Machine Learning from Disaster/test.csv")
+
+#探究数据集
+str(train)
+train <- train %>% mutate(Survived = factor(Survived),Pclass = factor(Pclass),
+                          Embarked = factor(Embarked),Sex = factor(Sex))
+str(test)
+test <- test %>% mutate(Pclass = factor(Pclass),Embarked = factor(Embarked),Sex = factor(Sex))
+
+# 合并train和test,test中不存在的列Survived会自动生成NA
+comb <- rbind(train,test)
+
+# 
