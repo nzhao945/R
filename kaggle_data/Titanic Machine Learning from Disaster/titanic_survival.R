567e95849dc7524bc3e1f36f105f5cc73e0e7ca4
nzhao945 <nzh0321@163.com>
2018-01-16 17:05:26 +0800

    mergan

--- kaggle_data/Titanic Machine Learning from Disaster/titanic_survival.R
+++ kaggle_data/Titanic Machine Learning from Disaster/titanic_survival.R
@@ -6,25 +6,32 @@ library(VIM)
 library(ggplot2)
 library(randomForest)
 
-#导入训练集和测试集
-train <- fread(header = T, stringsAsFactors = F,
+#导入训练集和测试集,发现部分空值不能正确读取为NA，故加入参数na.strings = c("NA","")
+train <- fread(header = T, stringsAsFactors = F,na.strings = c("NA","","N/A","null"),
                 file = "G:/R/kaggle_data/Titanic Machine Learning from Disaster/train.csv")
-test <- fread(header = T, stringsAsFactors = F,
+test <- fread(header = T, stringsAsFactors = F,na.strings = c("NA","","N/A","null"),
                 file = "G:/R/kaggle_data/Titanic Machine Learning from Disaster/test.csv")
 
-#探究数据集
+#了解数据集
 str(train)
 train <- train %>% mutate(Survived = factor(Survived),Pclass = factor(Pclass),
                           Embarked = factor(Embarked),Sex = factor(Sex))
 str(test)
 test <- test %>% mutate(Pclass = factor(Pclass),Embarked = factor(Embarked),Sex = factor(Sex))
 
-# 发现部分缺失值并没有被读取为NA，而是""，所以作如下操作
-train$Embarked[train$Embarked==""] <- NA
-train$Cabin[train$Cabin==""] <- NA
+#缺失值可视化
+aggr(train, prop = FALSE, combined = TRUE, numbers = TRUE, sortVars = TRUE, sortCombs = TRUE)
 
 # 合并train和test,test中不存在的列Survived由参数fill=T生成NA
 comb <- rbind(train,test, fill = T)
+#增加Title列
+comb$Title <- gsub('(.*, )|(\\..*)', '', comb$Name)
 
-#缺失值可视化
-aggr(train, prop = FALSE, combined = TRUE, numbers = TRUE, sortVars = TRUE, sortCombs = TRUE)
+###下面开始进行初步探索数据集###
+table(comb$Sex,comb$Title)
+rare_title <- c("Capt","Col","Don","Dona","Dr","Jonkheer","Lady","Major","Rev","Sir","the Countess")
+comb$Title[comb$Title == 'Mlle'] <- 'Miss' 
+comb$Title[comb$Title == 'Ms'] <- 'Miss'
+comb$Title[comb$Title == 'Mme'] <- 'Mrs' 
+comb$Title[comb$Title %in% rare_title] <- 'Rare Title'
+
