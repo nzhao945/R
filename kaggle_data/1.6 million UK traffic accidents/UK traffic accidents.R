73832328edf79fe3a5aed71ecf559bc6ae0ae34a
nzhao945 <nzh0321@163.com>
2018-03-22 20:22:24 +0800

    2nd trial

---
+++ kaggle_data/1.6 million UK traffic accidents/UK traffic accidents.R
@@ -0,0 +1,8 @@
+library(data.table)
+
+uk_traffic0507 <- fread(file = "D:/1.6_million_UK_traffic_accidents/accidents_2005_to_2007.csv",
+                        na.strings =c("NA","","N/A","null"), stringsAsFactors = F, header = T)
+uk_traffic0911 <- fread(file = 'D:/1.6_million_UK_traffic_accidents/accidents_2009_to_2011.csv',
+                        na.strings =c("NA","","N/A","null"), stringsAsFactors = F, header = T)
+uk_traffic1214 <- fread(file = 'D:/1.6_million_UK_traffic_accidents/accidents_2012_to_2014.csv',
+                        na.strings =c("NA","","N/A","null"), stringsAsFactors = F, header = T)
