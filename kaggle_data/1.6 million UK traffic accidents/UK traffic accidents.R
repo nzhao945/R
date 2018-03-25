83a6cdde6b26692d213e524086b00d96d4e19fa7
nzhao945 <nzh0321@163.com>
2018-03-25 16:34:49 +0800

    rdata

--- kaggle_data/1.6 million UK traffic accidents/UK traffic accidents.R
+++ kaggle_data/1.6 million UK traffic accidents/UK traffic accidents.R
@@ -1,4 +1,5 @@
 library(data.table)
+library(dplyr)
 
 uk_traffic0507 <- fread(file = "D:/1.6_million_UK_traffic_accidents/accidents_2005_to_2007.csv",
                         na.strings =c("NA","","N/A","null"), stringsAsFactors = F, header = T)
@@ -6,3 +7,11 @@ uk_traffic0911 <- fread(file = 'D:/1.6_million_UK_traffic_accidents/accidents_20
                         na.strings =c("NA","","N/A","null"), stringsAsFactors = F, header = T)
 uk_traffic1214 <- fread(file = 'D:/1.6_million_UK_traffic_accidents/accidents_2012_to_2014.csv',
                         na.strings =c("NA","","N/A","null"), stringsAsFactors = F, header = T)
+uk_traffic <- bind_rows(uk_traffic0507, uk_traffic0911, uk_traffic1214)
+ukTrafficAADF <- fread(file = '*D:/1.6_million_UK_traffic_accidents/ukTrafficAADF.csv',
+                       na.strings =c("NA","","N/A","null"), stringsAsFactors = F, header = T, integer64 = 'numeric')
+
+# 释放内存空间
+rm(uk_traffic0507, uk_traffic0911, uk_traffic1214)
+
+str(uk_traffic)
