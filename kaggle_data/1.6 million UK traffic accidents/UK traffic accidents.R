b05a31cb5623ee316058553fbf15dd592730a511
nzhao945 <nzh0321@163.com>
2018-03-26 08:53:54 +0800

    discard

--- kaggle_data/1.6 million UK traffic accidents/UK traffic accidents.R
+++ kaggle_data/1.6 million UK traffic accidents/UK traffic accidents.R
@@ -6,3 +6,4 @@ uk_traffic0911 <- fread(file = 'D:/1.6_million_UK_traffic_accidents/accidents_20
                         na.strings =c("NA","","N/A","null"), stringsAsFactors = F, header = T)
 uk_traffic1214 <- fread(file = 'D:/1.6_million_UK_traffic_accidents/accidents_2012_to_2014.csv',
                         na.strings =c("NA","","N/A","null"), stringsAsFactors = F, header = T)
+uk_traffic <- rbind(uk_traffic0507,uk_traffic0911,uk_traffic1214)
